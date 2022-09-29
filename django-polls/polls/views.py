b09d7063e970204f7e9c173bb566e75b522496a9
Chin Zhie Lun <zhie.lun.chin@fusionex.local>
2022-09-29 17:07:09 +0800

    Update

--- django-polls/polls/views.py
+++ django-polls/polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -12,15 +13,15 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
-
-
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
-
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
@@ -38,7 +39,5 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
+
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
